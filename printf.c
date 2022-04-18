7c8b3b8c53b7f170b6d3a26c45946e49ef6b5c20
VictorBK <victorbrian4@gmail.com>
2022-04-18 10:17:04 +0300

    Add comments

--- printf.c
+++ printf.c
@@ -3,11 +3,23 @@
 #include <unistd.h>
 #include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
 int _putchar(char c)
 {
 	return(write(1, &c, 1));
 }
 
+/**
+ * _printf - produces output according to a format
+ * @format: a pointer to the format string
+ *
+ * Return: on success, returns the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -35,12 +47,24 @@ int _printf(const char *format, ...)
 	return count;
 }
 
+/**
+ * print_percent - prints string
+ * @ap: argument pointer
+ *
+ * Return: number chars printed
+ */
 int print_percent(va_list ap)
 {
 	(void)ap;
 	return (_putchar('%'));
 }
 
+/**
+ * print_char - prints character
+ * @ap: argument pointer
+ *
+ * Retun: number chars printed
+ */
 int print_char(va_list ap)
 {
 	unsigned int sum = 0, ch = va_arg(ap, int);
@@ -48,6 +72,12 @@ int print_char(va_list ap)
 	return (sum);
 }
 
+/**
+ * get_specifier - fints the format function
+ * @s: the format string
+ *
+ * Return: the number of bytes printed
+ */
 int (*get_specifier(char *s))(va_list ap)
 {
 	specifier_t specifiers[] = {
@@ -70,6 +100,13 @@ int (*get_specifier(char *s))(va_list ap)
 	return (NULL);
 }
 
+/**
+ * get_print_func - finds the format func
+ * @s: the format string
+ * @ap: argument pointer
+ *
+ * Return: the number of bytes printed
+ */
 int get_print_func(char *s, va_list ap)
 {
 	int (*f)(va_list) = get_specifier(s);
@@ -80,6 +117,12 @@ int get_print_func(char *s, va_list ap)
 	return (0);
 }
 
+/**
+ * _puts - prints a string with newline
+ * @str: the string to print
+ *
+ * Return: int
+ */
 int _puts(char *str)
 {
 	char *a = str;
@@ -91,6 +134,12 @@ int _puts(char *str)
 	return (str - a);
 }
 
+/**
+ * print_string - prints string
+ * @ap: argument pointer
+ *
+ * Return: number chars printed
+ */
 int print_string(va_list ap)
 {
 	char *str = va_arg(ap, char *);
