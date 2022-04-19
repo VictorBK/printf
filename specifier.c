03801aefa5f81e01063d15e0e0b36dd5f8da88ed
Shirly Limo <shirleyjlimo@gmail.com>
2022-04-19 22:44:34 +0300

    fixed missing links to check

--- specifier.c
+++ specifier.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * get_specifier - finds the function of the format
+ * get_specifier - finds the format function
+ * get_specifier - prints the format function
  * @s: the format string
  *
  * Return: the number of bytes printed
